7111768d8d167eff2d7eef95438daae133b3fe6d
Niels Lange <info@nielslange.de>
2020-05-12 10:15:16 +0700

    Day 1 → Project: Toggle Password Visibility

---
+++ Day 01/README.md
@@ -0,0 +1,5 @@
+# Vanilla JS Academy » Day 1
+
+## Project: Toggle Password Visibility
+
+Create a script that toggles the visibility of a password input field in a login form.
---
+++ Day 01/script.js
@@ -0,0 +1,17 @@
+/* Final solution */
+
+const password = document.querySelector('#password');
+const toggle = document.querySelector('#show-password');
+
+function togglePassword() {
+	password.type = this.checked ? 'text' : 'password';
+}
+
+toggle.addEventListener( 'change', togglePassword );
+
+/* First solution */
+
+// document.querySelector('#show-password').addEventListener('change', () => {
+// 	let field = document.querySelector('#password');
+// 	field.type == 'password' ? field.setAttribute('type','text') : field.setAttribute('type','password');
+// });
