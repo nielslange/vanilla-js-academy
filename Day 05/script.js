8819c499cc4a8cee6fd7d333d8a7e02e3c01de64
Niels Lange <info@nielslange.de>
2020-05-17 21:06:37 +0700

    🔨 Refactor (D5)

--- Day 05/script.js
+++ Day 05/script.js
@@ -1,19 +1,30 @@
 // Define password toggle 
 function toggle( state, elements ) {
-	elements.forEach( item => { item.type = state == true ? 'text' : 'password'; } );
+
+	// Loop though all password fields
+	elements.forEach( item => {
+
+		// Toogle the input type
+		item.type = state == true ? 'text' : 'password'; 
+
+	} );
+
 }
 
-// Listen to all click events
+// Add event listener to all click events
 document.addEventListener( 'click', event => {
 
-	// Process sign up and change password forms
-	if ( event.target.matches( '#show-password' ) || event.target.matches( '#show-passwords' ) ) {
+	// Process if event matches "#show-password" and "#show-passwords"
+	if ( event.target.matches( '#show-password' ) || 
+			 event.target.matches( '#show-passwords' ) ) {
 		
 		// Get state of "Show passwords" checkbox
 		let state = event.target.checked;
 		
 		// Get password field(s) of corresponding form
-		let elements = Array.prototype.slice.call( event.target.parentNode.parentNode.querySelectorAll( '.field-password' ) );
+		let elements = Array.prototype.slice.call( 
+			event.target.parentNode.parentNode.querySelectorAll( '.field-password' ) 
+		);
 
 		// Toggle visibility of password field(s)
 		toggle( state, elements );
