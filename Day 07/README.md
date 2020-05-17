c55c483d85ce3c91875c14c66f178bedf238b581
Niels Lange <info@nielslange.de>
2020-05-17 15:12:01 +0700

    📦 Day 7 → Project: Character Count

---
+++ Day 07/README.md
@@ -0,0 +1,5 @@
+# Vanilla JS Academy » Day 7
+
+**Project: Character Count**
+
+Create a script that calculates teh number of characters inside a textarea.
---
+++ Day 07/script.js
@@ -0,0 +1,13 @@
+// Get text area field
+const text = document.querySelector('#text');
+
+// Get character count section
+const count = document.querySelector('#character-count');
+
+// Calculate number of characters and add numer to character count section
+function countCharacters() {
+	count.innerHTML = this.value.length;
+}
+
+// Listen to input events on text area field and call countCharacters() function
+text.addEventListener( 'input', countCharacters );
