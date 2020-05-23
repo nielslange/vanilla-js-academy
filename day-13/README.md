b5a5a04e1dd5a218f9f896022ab50dae6802461f
Niels Lange <info@nielslange.de>
2020-05-23 13:27:08 +0700

    📦 Day 13 → Project: Random Ron

---
+++ day-13/README.md
@@ -0,0 +1,11 @@
+# Vanilla JS Academy » Day 13
+
+## Project: Random Ron
+
+**Task:**
+
+Display a random Ron Swanson quote using the [Ron Swanson Quotes API](https://github.com/jamesseanwright/ron-swanson-quotes).
+
+**Demo:**
+
+https://nielslange.github.io/vanilla-js-academy/day-13/
---
+++ day-13/script.js
@@ -0,0 +1,23 @@
+// Get blockquote element.
+const blockquote = document.querySelector('blockquote > p');
+
+// Get button element.
+const button = document.querySelector('button');
+
+// Fetch a random Ron Swanson quote.
+function fetchQuote() {
+	// Fetch a random quote from http://ron-swanson-quotes.herokuapp.com/v2/quotes
+	fetch( 'http://ron-swanson-quotes.herokuapp.com/v2/quotes' ).then( response => {
+		// Return quote on success or error on failure.
+		return response.ok ? response.json() : Promise.reject(response);
+	}).then( data => {
+		// Add quopte to blockquote element.
+		blockquote.textContent = data;
+	}).catch( error => {
+		// Catch and print error it in console.
+		console.error(error);
+	});
+}
+
+// Listen to button click.
+button.addEventListener( 'click', fetchQuote );
