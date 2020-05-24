16fc071b18490ee44f93c6c346baddd3e08d29aa
Niels Lange <info@nielslange.de>
2020-05-24 17:26:21 +0700

    🔨 Refactor (D13)

--- day-13/script.js
+++ day-13/script.js
@@ -4,20 +4,39 @@ const blockquote = document.querySelector('blockquote > p');
 // Get button element.
 const button = document.querySelector('button');
 
+// Define the API URL.
+const url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+
+// Get JSON response 
+function getJSON(response) {
+	return response.ok ? response.json() : Promise.reject(response);
+}
+
+// Add random quote to blockquote element.
+function showQuote(data) {
+	blockquote.textContent = data;
+}
+
+// Show error message.
+function showError(error) {
+  blockquote.parentNode.outerHTML = (
+		`<div class="alert alert-danger" role="alert">
+			Sorry, there was a problem getting your
+			Ron Swanson quote! Please try again later.
+		</div>`
+  );
+}
+
 // Fetch a random Ron Swanson quote.
 function fetchQuote() {
-	// Fetch a random quote from https://ron-swanson-quotes.herokuapp.com/v2/quotes
-	fetch( 'https://ron-swanson-quotes.herokuapp.com/v2/quotes' ).then( response => {
-		// Return quote on success or error on failure.
-		return response.ok ? response.json() : Promise.reject(response);
-	}).then( data => {
-		// Add quopte to blockquote element.
-		blockquote.textContent = data;
-	}).catch( error => {
-		// Catch and print error it in console.
-		console.error(error);
-	});
+	fetch(url)
+	.then(getJSON)
+	.then(showQuote)
+	.catch(showError);
 }
 
 // Listen to button click.
-button.addEventListener( 'click', fetchQuote );
+button.addEventListener('click', fetchQuote);
+
+// Call fetchQuote() when page is fully loaded.
+document.addEventListener('DOMContentLoaded', fetchQuote)
