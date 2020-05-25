eeba6152c2a9d41e54e58d75981a0f7af51f1021
Niels Lange <info@nielslange.de>
2020-05-25 16:12:16 +0700

    📦 Day 15 → Project: Random Ron without duplicates

---
+++ day-15/README.md
@@ -0,0 +1,11 @@
+# Vanilla JS Academy » Day 15
+
+## Project: Random Ron without duplicates
+
+**Task:**
+
+Display a random Ron Swanson quote using the [Ron Swanson Quotes API](https://github.com/jamesseanwright/ron-swanson-quotes) and ensure not to show duplicates for teh last 50 quotes.
+
+**Demo:**
+
+https://nielslange.github.io/vanilla-js-academy/day-15/
---
+++ day-15/script.js
@@ -0,0 +1,70 @@
+// Get blockquote element.
+const blockquote = document.querySelector('blockquote > p');
+
+// Get button element.
+const button = document.querySelector('button');
+
+// Define the API URL.
+const url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+
+// Define empty array for quotes.
+const quotes = [];
+
+// Define number of unique consecutive quotes.
+const unique_quotes = 5;
+
+// Get JSON response 
+function getJSON(response) {
+	return response.ok ? response.json() : Promise.reject(response);
+}
+
+// Add random quote to blockquote element.
+function showQuote(data) {
+	blockquote.textContent = getUniqueQuote(data);
+}
+
+// Return unique quote.
+function getUniqueQuote(data) {
+	// Get current quote.
+	const quote = data[0];
+
+	// If current quote is in array, get a new quote.
+	if (quotes.includes(quote)) {
+		fetchQuote();
+	}
+
+	// If array has 50 quotes, remove the first quote.
+	if (quotes.length === unique_quotes) {
+		quotes.shift();
+	}
+
+	// Add the new quote to the array.
+	quotes.push(quote);
+	
+	// Return the quote
+	return quote;
+}
+
+// Show error message.
+function showError(error) {
+  blockquote.parentNode.outerHTML = (
+		`<div class="alert alert-danger" role="alert">
+			Sorry, there was a problem getting your
+			Ron Swanson quote! Please try again later.
+		</div>`
+  );
+}
+
+// Fetch a random Ron Swanson quote.
+function fetchQuote() {
+	fetch(url)
+	.then(getJSON)
+	.then(showQuote)
+	.catch(showError);
+}
+
+// Listen to button click.
+button.addEventListener('click', fetchQuote);
+
+// Call fetchQuote() when page is fully loaded.
+document.addEventListener('DOMContentLoaded', fetchQuote)
