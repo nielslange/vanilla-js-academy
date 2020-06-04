0812b16ac145eb655b969e60e23aef5e329c45cb
Niels Lange <info@nielslange.de>
2020-06-04 21:00:01 +0700

    📦 Day 25 → Project: Find the Monsters

--- day-25/script.js
+++ day-25/script.js
@@ -45,9 +45,9 @@ let checkedBarrels = 0;
 		currentIndex -= 1;
 
 		// And swap it with the current element.
-		temporaryValue 			= array[currentIndex];
+		temporaryValue      = array[currentIndex];
 		array[currentIndex] = array[randomIndex];
-		array[randomIndex] 	= temporaryValue;
+		array[randomIndex]  = temporaryValue;
 	}
 
 	return array;
