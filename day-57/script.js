521c0c272c77a1255925d57b0508fe5ba85a0461
Niels Lange <info@nielslange.de>
2020-07-08 13:12:16 +0200

    🔨 Refactor (D57)

--- day-57/script.js
+++ day-57/script.js
@@ -1,28 +1,18 @@
+// Define duration.
+const duration = 5;
+let timer;
+
 /**
  * Define constructor pattern
  *
- * @param {String} selector
- * @param {String} options
+ * @param {String} selector The selector of the target element.
+ * @param {String} options The options of the component.
  */
 const Rue = function ( selector, options ) {
 	this.elem = document.querySelector( selector );
 	this.data = options.data;
 	this.template = options.template;
 };
-
-/**
- * Initialize app
- */
-const app = new Rue( '#app', {
-	// Define data.
-	data: {
-		time: 60,
-	},
-	// Define functions.
-	template: function ( props ) {
-		return `${ props.time }`;
-	},
-} );
 
 /**
  * Define render function
@@ -34,14 +24,77 @@ Rue.prototype.render = function () {
 /**
  * Define interval to count down
  */
-const countDown = window.setInterval( function () {
-	// Show time.
-	app.render();
-
-	// Decrease time;
+const countdown = function () {
+	// Decrease time by 1 second;
 	app.data.time--;
 
-	if ( 0 > app.data.time ) {
-		window.clearInterval( countDown );
-	}
-}, 1000 );
+	// Check of the timer should be stopped.
+	stopTimer();
+
+	// Update the UI.
+	app.render();
+};
+
+/**
+ * Start the timer
+ */
+const startTimer = function () {
+	// Reset the app data.
+	app.data.time = duration;
+
+	// Render the initial UI.
+	app.render();
+
+	// Start the countdown timer.
+	timer = setInterval( countdown, 1000 );
+};
+
+/**
+ * Stop the timer
+ */
+const stopTimer = function () {
+	if ( 0 < app.data.time ) return;
+	clearInterval( timer );
+};
+
+/**
+ * Handle click events
+ *
+ * @param {Object} event The event object.
+ */
+const clickHandler = function ( event ) {
+	// Only run, if the restart button was clicked.
+	if ( ! event.target.hasAttribute( 'data-restart-timer' ) ) return;
+
+	// Start the timer.
+	startTimer();
+};
+
+/**
+ * Create the timer component
+ *
+ * @param {Object} selector The selector of the target element.
+ */
+const app = new Rue( '#app', {
+	// Define data.
+	data: {
+		time: duration,
+	},
+
+	// Define functions.
+	template: function ( props ) {
+		// Show restart button, if timer is done.
+		if ( 1 > props.time ) {
+			return '⏰ <p><button class="btn btn-primary" data-restart-timer>Restart timer</button></p>';
+		}
+
+		// Show the current time, if the timer is still running.
+		return props.time;
+	},
+} );
+
+// Start the timer.
+startTimer();
+
+// Listen to click events.
+document.addEventListener( 'click', clickHandler );
